b3a1faf05cdc611d28bf688805ab19b76fad9ed0
eduardoacost <andres_edu.acosta@uao.edu.co>
2023-10-01 11:53:23 -0500

    Mapa Argentina Primera parte + interfaces , de pausa, tareas , ayuda y opciones

---
+++ Assets/Scripts/MenuP.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuP : MonoBehaviour
+{
+    public GameObject menuPausaUI;
+    public GameObject configuracionUI;
+    public GameObject AyudaUI;
+    public GameObject TareasUI;
+    public GameObject Tareas2UI;
+    
+
+    private bool juegoPausado = false;
+
+    private void Start()
+    {
+        menuPausaUI.SetActive(false);
+        configuracionUI.SetActive(false);
+        AyudaUI.SetActive(false);
+        TareasUI.SetActive(false);
+        Tareas2UI.SetActive(false);
+
+    }
+
+    public void PausarJuego()
+    {
+        menuPausaUI.SetActive(true);
+        Time.timeScale = 0f;
+        juegoPausado = true;
+    }
+
+    public void ReanudarJuego()
+    {
+        menuPausaUI.SetActive(false);
+        configuracionUI.SetActive(false); // Asegúrate de ocultar la interfaz de configuración si estaba abierta.
+        Time.timeScale = 1f;
+        juegoPausado = false;
+    }
+
+    public void AbrirConfiguracion()
+    {
+        configuracionUI.SetActive(true);
+        menuPausaUI.SetActive(false);
+    }
+       public void CerrarConfiguracion()
+    {
+        configuracionUI.SetActive(false);
+        menuPausaUI.SetActive(true);
+    }
+
+    public void SalirDelJuego()
+    {
+        Application.Quit(); // Esto cierra la aplicación (debes estar en modo de compilación para ejecutables).
+    }
+
+    public void SalirDelMenuPausa()
+    {
+        menuPausaUI.SetActive(false);
+    }
+    public void SalirDelConfig()
+    {
+        configuracionUI.SetActive(false);
+    }
+     public void Abriraiuda()
+    {
+        AyudaUI.SetActive(true);
+       
+    }
+     public void Cerraraiuda()
+    {
+        AyudaUI.SetActive(false);
+       
+    }
+     public void AbrirTareas()
+    {
+        AyudaUI.SetActive(false);
+        TareasUI.SetActive(true);
+        Tareas2UI.SetActive(false);
+    }
+    public void CerrarTareas()
+    {
+        AyudaUI.SetActive(false);
+        TareasUI.SetActive(false);
+        Tareas2UI.SetActive(false);
+    }
+     public void AbrirTareas2()
+    {
+        TareasUI.SetActive(false);
+        Tareas2UI.SetActive(true);
+    }
+    public void CerrarTareas2()
+    {
+        AyudaUI.SetActive(false);
+        TareasUI.SetActive(false);
+        Tareas2UI.SetActive(false);
+    }
+     
+}
+
